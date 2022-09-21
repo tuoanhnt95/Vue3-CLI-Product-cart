712544a1b5780430c3ca8e004967557218783f62
Oanh Nguyen <myspamvn@yahoo.com>
2022-09-21 17:52:17 +0900

    build: Add routers for three pages2

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,17 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/past-orders',
+    name: 'past-orders',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/PastOrdersView.vue')
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: () => import('../views/ProductsView.vue')
   }
 ]
 
