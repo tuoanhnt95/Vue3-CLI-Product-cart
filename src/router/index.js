9acb3966bbcbc26e45718f89f82241ba62408045
Oanh Nguyen <myspamvn@yahoo.com>
2022-09-21 18:24:37 +0900

    feat: Add Scss styling (error) Error with nodes_module npm file.

--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,7 @@ const routes = [
   {
     path: '/past-orders',
     name: 'past-orders',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/PastOrdersView.vue')
+    component: () => import('../views/PastOrdersView.vue')
   },
   {
     path: '/products',
